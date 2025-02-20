55aeae0b679bbb48d70bab450ae3a9a69c8f3659
Julie Bissø <U2507981@rts-365.dk>
2025-02-20 10:26:32 +0100

    layyout of homescreen

---
+++ home.js
@@ -0,0 +1,13 @@
+
+
+
+let sectionElm = document.createElement("section")
+sectionElm.classList="home_title"
+
+sectionElm.innerHTML +=
+`
+    <h1>Pokèdex</h1>
+
+`
+
+document.querySelector("header").append(sectionElm)
