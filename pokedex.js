46715d8a0678035727851a40642f58ef0caa216f
Julie Bissø <U2507981@rts-365.dk>
2025-02-25 18:47:22 +0100

    idk what i've done

--- pokedex.js
+++ pokedex.js
@@ -1,50 +1,20 @@
-let SectionElm = document.createElement("section")
-sectionElm.classList="pokelist"
-document.querySelector("main").append(SectionElm)
-SectionElm.innerHTML =
-`
-<div>
-<p>id</p>
-<a href=""><img src="" alt="bing bong"></a>
-<h4>name of pokèmon</h4>
-</div>
-`
+let SectionElm = document.createElement("section");
+SectionElm.classList="pokelist";
 document.querySelector("main").append(SectionElm)
 
 
-
-//? link to api first 20
-/* "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
- */
-
-
-
-
-
-
-
-
-
-/* let SectionElm = document.createElement("section")
-SectionElm.classList="pokelist"
-
-document.querySelector("main").append(SectionElm)
-fetch("/data/pokedex.json")
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20")
     .then(response => response.json())
+
     .then(data => {
 
-        SectionElm.innerHTML = data.pokedex.map(pokemon =>
+        SectionElm.innerHTML = data.map(pokemon => {
+`    <p>${pokemon.name}</p>
 `
-<div>
-<p>id</p>
-<a href="pokemon.html?id="${pokemon.name}"><img src="/${pokemon.image}" alt="bing bong"></a>
-<h4>name of pokèmon</h4>
-</div>
-`
-).join("")
 
+})
 document.querySelector("main").append(SectionElm)
-    }) */ //?test for fetch
+})
 
 
 
